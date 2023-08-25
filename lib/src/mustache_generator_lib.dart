f8b5605f913c920278affe72e58fcdc8b12bc54d
Adam Langley <github.com@irisdesign.co.nz>
2023-08-25 12:51:25 +1200

    Update mustache_generator_lib.dart

--- lib/src/mustache_generator_lib.dart
+++ lib/src/mustache_generator_lib.dart
@@ -21,7 +21,7 @@ class MustacheLibGenerator implements Builder {
   @override
   Map<String, List<String>> get buildExtensions {
     return {
-      '\$lib${Platform.pathSeparator}gen${Platform.pathSeparator}\$': ['templates_output.dart', 'templates_decorators.dart'],
+      r'$lib/gen$': ['templates_output.dart', 'templates_decorators.dart'],
     };
   }
 
